c9a2168f70c44e976e16e89bc1d826349fd2cbdd
oartuu <artu.donascimento@gmail.com>
2026-05-20 20:11:09 -0300

    hotfix(list):update redirect url

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -35,7 +35,7 @@ export class ListController {
     const validationToken = await this.listService.create_validation_token()
 
     return {
-      url: `http://localhost:3001/list/send/${token}/${validationToken}`,
+      url: `https://beacon4u.vercel.app/list/send/${token}/${validationToken}`,
     };
     
   }
